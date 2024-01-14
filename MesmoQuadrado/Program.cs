522a3646202a8cb0140b845964fc7373811a60ad
Danilo-dps <danilodpsnilo@outlook.com>
2024-01-14 19:27:30 -0300

    Números ao quadrado

---
+++ MesmoQuadrado/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i <= n; i++)
+{
+    if(i % 2 == 0 && i > 0)
+    {
+        Console.Write($"{i}^2 = ");
+        Console.WriteLine(Math.Pow(i, 2));
+    }
+}
