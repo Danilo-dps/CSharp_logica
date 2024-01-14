d73689d4d163a89cdf0d1628a5f2027cfdc297dd
Danilo-dps <danilodpsnilo@outlook.com>
2024-01-14 11:13:57 -0300

    Switch-case com dias da semana

---
+++ DiaDaSemana/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Digite entre [1] e [7] para escolher um dia da semana");
+
+int x = int.Parse(Console.ReadLine());
+string dia;
+
+switch(x){
+    case 1:
+    dia = "Domingo";
+    break;
+
+    case 2:
+    dia = "Segunda";
+    break;
+
+    case 3: 
+    dia = "Terça";
+    break;
+
+    case 4: 
+    dia ="Quarta";
+    break;
+
+    case 5: 
+    dia = "Quinta";
+    break;
+
+    case 6:
+    dia = "Sexta";
+    break;
+
+    case 7:
+    dia = "Sábado";
+    break;
+
+    default:
+    dia = "Valor Inválido";
+    break;
+}
+
+Console.WriteLine("Dia da semana: " + dia);
+
