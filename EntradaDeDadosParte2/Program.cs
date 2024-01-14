acb72b93a2fe82996514da3c25f7f8464ee6a0b0
Danilo-dps <danilodpsnilo@outlook.com>
2024-01-14 10:45:56 -0300

    Foi adicionado o código referente ao nome da pasta

--- EntradaDeDadosParte2/Program.cs
+++ EntradaDeDadosParte2/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Globalization;//para usar o . como separador de decimal
+
+int n1 = int.Parse(Console.ReadLine());
+char ch = char.Parse(Console.ReadLine());
+double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);//para usar o . como separador de decimal
+
+string [] vet = Console.ReadLine().Split(' ');
+string nome = vet[0];
+char sexo = char.Parse(vet[1]);
+int idade = int.Parse(vet[2]);
+double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+Console.WriteLine("Você digitou: ");
+Console.WriteLine(n1);
+Console.WriteLine(ch);
+Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine(nome);
+Console.WriteLine(sexo);
+Console.WriteLine(idade);
+Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
