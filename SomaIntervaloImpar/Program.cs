c0f33576c4888f834bebae7ced63b7e9aff21196
Danilo-dps <danilodpsnilo@outlook.com>
2024-01-14 18:48:00 -0300

    1º commit - não está reconhecendo números negativos

---
+++ SomaIntervaloImpar/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+int soma = 0;
+if(y > x)
+{
+  for(; y > x; x++)
+        {
+          if(x % 2 != 0)
+          {
+            soma = soma + (x);
+          }  
+        }      
+}
+else if(x > y)
+{
+  for(; x > y; y++)                         
+        {
+          if(y % 2 != 0)
+          {
+            soma = soma + (y);
+          }
+        }    
+}
+Console.WriteLine(soma);
+
+ 
