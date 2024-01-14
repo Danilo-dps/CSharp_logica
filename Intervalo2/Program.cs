db41d8d071fbca8c3f04807ab1f3524d56664c1c
Danilo-dps <danilodpsnilo@outlook.com>
2024-01-14 18:45:01 -0300

    1º commit - Não está reconhecendo os números negativos

---
+++ Intervalo2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+Console.WriteLine("Digite um número: ");
+int n = int.Parse(Console.ReadLine());
+int cont = 0;
+int count = 0;
+
+for(int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    if(10 < x && x < 20)
+    {
+        cont++;
+    }
+    else
+    {
+        count++;
+    }
+}
+Console.WriteLine(cont + " in");
+Console.WriteLine(count + " out");
+
+
--- Intervalo2/obj/Debug/net8.0/Intervalo2.AssemblyInfo.cs
+++ Intervalo2/obj/Debug/net8.0/Intervalo2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("DiaDaSemana")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Intervalo2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62b20fee1d6b2a7c89f29051cb60dced6f061a4d")]
-[assembly: System.Reflection.AssemblyProductAttribute("DiaDaSemana")]
-[assembly: System.Reflection.AssemblyTitleAttribute("DiaDaSemana")]
+[assembly: System.Reflection.AssemblyProductAttribute("Intervalo2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Intervalo2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
