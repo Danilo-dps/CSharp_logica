a3124954452c0c9f1c5fb47db2af2d3535df27e7
Danilo-dps <danilodpsnilo@outlook.com>
2024-01-14 10:48:07 -0300

    Menor de três

---
+++ MenorDeTres/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+int x, y, z;
+
+string[] vet = Console.ReadLine().Split(' ');
+
+x = int.Parse(vet[0]);
+y = int.Parse(vet[1]);
+z = int.Parse(vet[2]);
+
+int menor = x;
+if(y < x && y < z)
+{
+    menor = y;
+}
+else if(x > z)
+{
+    menor = z;
+}
+
+Console.WriteLine("MENOR = " + menor);
+
