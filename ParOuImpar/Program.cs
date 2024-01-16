821e2febbe671bc9d66044e7c3c2192250c330e6
Danilo-dps <danilodpsnilo@outlook.com>
2024-01-15 21:10:51 -0300

    Par Positivo ou Ímpar Positivo ou Par Negativo ou Ímpar Negativo

---
+++ ParOuImpar/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+int n = int.Parse(Console.ReadLine());
+
+int a = 0;
+
+for(int i = 0; i < n; i++)
+{
+   a = int.Parse(Console.ReadLine());
+  if(a == 0)
+  {
+    Console.WriteLine("NULL");
+  }
+  else
+  {
+    if(a > 0 && a % 2 == 0)
+    {
+        Console.WriteLine("EVEN POSITIVE");
+    }
+    else if(a < 0 && a % 2 == 0)
+    {
+        Console.WriteLine("EVEN NEGATIVE");
+    }
+    else if(a > 0 && a % 2 != 0)
+    {
+        Console.WriteLine("ODD POSITIVE");
+    }
+    else
+    {
+        Console.WriteLine("ODD NEGATIVE");
+    }
+  }
+}
+
+    
+    
+
+
+ //int[] a = new int [n]; COMO DECLARAR UM VETOR
