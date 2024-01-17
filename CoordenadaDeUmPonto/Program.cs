7cb0b66b5df2b52757c2ccb3170fdd88f03c1cf9
Danilo-dps <danilodpsnilo@outlook.com>
2024-01-17 00:20:40 -0300

    Coordenadas no plano cartesiano

---
+++ CoordenadaDeUmPonto/Program.cs
@@ -0,0 +1,32 @@
+﻿string[] coordenadas = Console.ReadLine().Split(' ');
+float x = Convert.ToSingle(coordenadas[0]);
+float y = Convert.ToSingle(coordenadas[1]);
+
+if(x > 0 && y > 0)
+{
+    Console.WriteLine("Q1");
+}
+else if(x < 0 && y > 0)
+{
+    Console.WriteLine("Q2");
+}
+else if(x < 0 && y < 0)
+{
+    Console.WriteLine("Q3");
+}
+else if(x > 0 && y < 0)
+{
+    Console.WriteLine("Q4");
+}
+else if(x > 0 || x < 0 && y == 0)
+{
+    Console.WriteLine("Eixo X");
+}
+else if(x == 0 && y > 0 || y < 0)
+{
+    Console.WriteLine("Eixo Y");
+}
+else
+{
+    Console.WriteLine("Origem");
+}
