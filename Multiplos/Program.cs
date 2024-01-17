c8438cdd96f3769fee2c1428edfb522c0b0cdcf3
Danilo-dps <danilodpsnilo@outlook.com>
2024-01-17 07:41:23 -0300

    Múltiplos ou não

---
+++ Multiplos/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string [] ab = Console.ReadLine().Split(' ');
+int a = Convert.ToInt32(ab[0]);
+int b = Convert.ToInt32(ab[1]);
+
+if(a % b == 0 || b % a == 0)
+{
+    Console.WriteLine("São Multiplos");
+}
+else
+{
+    Console.WriteLine("Não são Múltiplos");
+}
