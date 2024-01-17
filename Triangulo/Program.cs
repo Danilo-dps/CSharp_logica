3d522cb13f2c1c3ccbffebcb968009267528c1e6
Danilo-dps <danilodpsnilo@outlook.com>
2024-01-17 02:01:18 -0300

    Perimetro Triângulo ou Área Trapézio

---
+++ Triangulo/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] medidas = Console.ReadLine().Split(' ');
+float a = Convert.ToSingle(medidas[0]);
+float b = Convert.ToSingle(medidas[1]);
+float c = Convert.ToSingle(medidas[2]);
+float trapezio = (a + b) * c / 2;
+
+if(a + b > c && a + c > b && b + c > a)
+{
+    Console.WriteLine($"Perimetro = {(a + b + c).ToString("F1")}"); 
+
+}
+else
+{
+    Console.WriteLine($"Area = {Math.Floor(trapezio * 10) / 10:F1}");
+}
