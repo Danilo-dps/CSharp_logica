422290c58d630bb9f1ee6de410f2f7e3ccfde3ad
Danilo-dps <danilodpsnilo@outlook.com>
2024-01-18 00:14:26 -0300

    Definições de animais ou insetos

---
+++ Animal/Program.cs
@@ -0,0 +1,43 @@
+﻿string a = Console.ReadLine();
+string b = Console.ReadLine();
+string c = Console.ReadLine();
+
+if(a == "vertebrado" )
+{
+    if( b == "ave" && c == "carnivoro")
+    {
+        Console.WriteLine("aguia");
+    }
+    else if(b == "mamifero" && c == "onivoro")
+    {
+        Console.WriteLine("homem");
+    }
+    else if(b == "mamifero" && c == "herbivoro")
+    {
+        Console.WriteLine("vaca");
+    }
+    else
+    {
+        Console.WriteLine("pomba");
+    }
+
+}
+else if(a == "invertebrado")
+{
+    if(b == "inseto" && c == "hematofago")
+    {
+        Console.WriteLine("pulga");
+    }
+    else if(b == "inseto" && c == "herbivoro")
+    {
+        Console.WriteLine("lagarta");
+    }
+    else if(b == "anelideo" && c == "hematofago")
+    {
+        Console.WriteLine("sanguessuga");
+    }
+    else
+    {
+        Console.WriteLine("minhoca");
+    }
+}
