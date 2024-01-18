a7237385e34d2948e953fe41aa23222aa45e127f
Danilo-dps <danilodpsnilo@outlook.com>
2024-01-17 21:20:34 -0300

    Tempo de jogo

---
+++ TempoDeJogo/Program.cs
@@ -0,0 +1,14 @@
+﻿string [] tempo = Console.ReadLine().Split(' ');
+int inicio = Convert.ToInt32(tempo[0]);
+int fim = Convert.ToInt32(tempo[1]);
+int duracao = 0;
+if(inicio < fim)
+{
+   duracao = fim - inicio;
+}
+else
+{
+   duracao = (24 - inicio) + fim;
+}
+
+Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
