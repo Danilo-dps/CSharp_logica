283f713dd9993f29d5a01c7caaacd5504238c5e0
Danilo-dps <danilodpsnilo@outlook.com>
2024-01-18 00:51:27 -0300

    Escolha de DDDs e a apresentação da cidade correspondente

---
+++ DDD/Program.cs
@@ -0,0 +1,43 @@
+﻿int ddd = Convert.ToInt32(Console.ReadLine());
+string escolha;
+
+switch(ddd)
+{
+    case 11:
+    escolha = "Sao Paulo";
+    break;
+
+    case 19:
+    escolha = "Campinas";
+    break;
+
+    case 21:
+    escolha = "Rio de Janeiro";
+    break;
+
+    case 27:
+    escolha = "Vitoria";
+    break;
+
+    case 31:
+    escolha = "Belo Horizonte";
+    break;
+
+    case 32:
+    escolha = "Juiz de Fora";
+    break;
+
+    case 61:
+    escolha = "Brasilia";
+    break;
+
+    case 71:
+    escolha = "Salvador";
+    break;
+
+    default: 
+    escolha = "DDD nao cadastrado";
+    break;
+}
+
+Console.WriteLine(escolha);
