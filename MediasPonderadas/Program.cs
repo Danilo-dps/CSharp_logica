a1b6c0c0488f36248b6c004beb3292278d35cf10
Danilo-dps <danilodpsnilo@outlook.com>
2024-01-21 12:59:48 -0300

    Média ponderadas de 3 números dentro de um loop for

---
+++ MediasPonderadas/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 0; i < n; i++)
+{
+   string[] valores = Console.ReadLine().Split(' ');
+   double a = Convert.ToDouble(valores[0]) * 2;
+   double b = Convert.ToDouble(valores[1]) * 3;
+   double c = Convert.ToDouble(valores[2]) * 5;
+   double media = (a + b + c) / 10;
+
+   Console.WriteLine($"{media.ToString("F1")}");
+}
