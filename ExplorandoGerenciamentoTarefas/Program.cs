0a65bb56762ba70202ad08c8208d01625262fc81
danilo pereira da silva <danilodpsnilo@outlook.com>
2024-03-29 22:56:31 -0300

    Gerenciando tarefas

---
+++ ExplorandoGerenciamentoTarefas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        // TODO: Crie as variáveis titulo, descricao e dataVencimento para guardar as informações das tarefas:
+
+
+        // Obtém o titulo e a descricao a partir da entrada do console  
+        string titulo = Console.ReadLine();
+        string descricao = Console.ReadLine();
+
+        // TODO: Crie uma estrutura condicional 'if/else' para verificar se a descrição da tarefa excede 50 caracteres.
+        int maxTamanho = 50;
+        if(descricao.Length > maxTamanho)
+        {
+            Console.WriteLine("Descricao ultrapassa limite de caracteres.");
+        }
+        else
+        {
+        // Caso a descricao esteja dentro do limite é solicitado a entrada pelo console:
+        string dataVencimento = Console.ReadLine();
+        // TODO: Imprima a descricao e a data de vencimento como nos exemplos da tabela:            
+        Console.WriteLine($"{descricao} ate {dataVencimento}");
+        }  
+    }
+}
